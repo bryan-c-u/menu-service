d1372cc4e6395d8d56f93c6355b3b1f7f71246b4
krisgallego <krisgallego7@gmail.com>
2026-03-19 18:25:03 -0500

    creacion dto

--- src/main/java/com/restaurante/menu_service/dto/DishResponseDTO.java
+++ src/main/java/com/restaurante/menu_service/dto/DishResponseDTO.java
@@ -1,5 +1,59 @@
 package com.restaurante.menu_service.dto;
 
 public class DishResponseDTO {
-    
+
+    private Integer id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean available;
+    private int stock;
+
+    public Integer getId() { 
+        return id; 
+    }
+
+    public void setId(Integer id) { 
+        this.id = id; 
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public String getDescription() { 
+        return description; 
+    }
+
+    public void setDescription(String description) { 
+        this.description = description; 
+    }
+
+    public double getPrice() { 
+        return price; 
+    }
+
+    public void setPrice(double price) { 
+        this.price = price; 
+    }
+
+    public boolean isAvailable() { 
+        return available; 
+    }
+
+    public void setAvailable(boolean available) { 
+        this.available = available; 
+    }
+
+    public int getStock() { 
+        return stock; 
+    }
+
+    public void setStock(int stock) { 
+        this.stock = stock; 
+    }
 }
--- src/main/java/com/restaurante/menu_service/repository/DishRepository.java
+++ src/main/java/com/restaurante/menu_service/repository/DishRepository.java
@@ -3,5 +3,5 @@ package com.restaurante.menu_service.repository;
 import com.menu.service.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DishRepository extends JpaRepository<Dish, Integer> {
+public interface DishRepository extends JpaRepository<Dish, Long> {
 }
